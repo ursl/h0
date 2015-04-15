f35736ac94716948df40dcb25763a67e1cbc6325
Urs Langenegger <urs.langenegger@psi.ch>
2015-04-15 11:13:43 +0200

    add missing std::

--- plotHpt.hh
+++ plotHpt.hh
@@ -29,7 +29,7 @@ public :
   void   makeAll(int bitmask = 0);
   void   plot1(std::string what = "pt", double xmin = 200., double xmax = 1000.); 
   void   plot2(); 
-  virtual void treeAnalysis(int mask = 1, int nevts = -1, string opt = "RECREATE", string ds = ""); 
+  virtual void treeAnalysis(int mask = 1, int nevts = -1, std::string opt = "RECREATE", std::string ds = ""); 
   void   massResolution(std::string hname = "mpt_mcatnlo5_nopt");
   // -- these two are historic 
   void   optimizeCuts(std::string fname = "opt.root", double lumi = 1000., int nevts = -1);
