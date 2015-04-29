65a80e03d894d0458b2800520ec6d53391567549
Urs Langenegger <urs.langenegger@psi.ch>
2015-04-29 11:15:38 +0200

    add hstat

--- runPlot.cc
+++ runPlot.cc
@@ -14,10 +14,12 @@
 #include "TUnixSystem.h"
 
 #include "plotHpt.hh"
+#include "hstat.hh"
 
 using namespace std;
 
 // bin/runPlot -s 1 -c "PTLO=400;PTHI=10000;G0PT=100;g1PT=40"
+// bin/runPlot -s 0 -a hstat -m 0 -n 200
 
 // ----------------------------------------------------------------------
 int main(int argc, char *argv[]) {
@@ -26,11 +28,13 @@ int main(int argc, char *argv[]) {
   string dir("hpt0"), 
     ifiles("plotHpt.files"), 
     setup(""), 
-    cuts("nada");
+    cuts("nada"),
+    ana("plotHpt");
   
-  int mode(-1), ntoy(200), rndms(111); 
+  int mode(-1), ntoy(2000), rndms(111); 
   // -- command line arguments
   for (int i = 0; i < argc; i++){
+    if (!strcmp(argv[i], "-a"))  {ana    = string(argv[++i]);}
     if (!strcmp(argv[i], "-c"))  {cuts   = string(argv[++i]);}
     if (!strcmp(argv[i], "-d"))  {dir    = string(argv[++i]);}
     if (!strcmp(argv[i], "-f"))  {ifiles = string(argv[++i]);}
@@ -42,17 +46,24 @@ int main(int argc, char *argv[]) {
 
 
   gROOT->Clear();  gROOT->DeleteAll();
-  plotHpt a(dir, ifiles, setup);
-  if (cuts != "nada") {
-    cout << "setting cuts: " << cuts << endl;
-    a.setCuts(cuts);
-  } 
-  a.setNtoy(ntoy); 
-  a.setRndmSeed(rndms); 
-  if (mode > -1) {
-    a.makeAll(mode);
-  } else {
-    a.makeAll(1); 
-  } 
 
+  if (ana == "plotHpt") {
+    plotHpt a(dir, ifiles, setup);
+    if (cuts != "nada") {
+      cout << "setting cuts: " << cuts << endl;
+      a.setCuts(cuts);
+    } 
+    a.setNtoy(ntoy); 
+    a.setRndmSeed(rndms); 
+    if (mode > -1) {
+      a.makeAll(mode);
+    } else {
+      a.makeAll(1); 
+    } 
+  } else if (ana == "hstat") {
+    hstat a; 
+    if ("0" == setup) {
+      a.run1D(ntoy, mode);
+    }
+  }
 }
